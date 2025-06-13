82bb6cac580f8ac615ad3dd011d0c3c5a15285c0
sylesh7 <sylesh.27it@licet.ac.in>
2025-06-13 18:39:06 +0530

    NEXT CONFIG Commit

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
+  webpack: (config: { resolve: { fallback: { fs: boolean; net: boolean; tls: boolean; }; }; externals: string[]; }) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push('pino-pretty', 'encoding');
     return config;
