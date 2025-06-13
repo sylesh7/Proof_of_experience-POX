c1819ccaf5b5ad3f5f0b423f38fb0144724580bc
Suganthan9906 <suganthan.27it@licet.ac.in>
2025-06-14 00:34:13 +0530

    Feat: Added a img and positioned a navbar

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   webpack: (config: { resolve: { fallback: { fs: boolean; net: boolean; tls: boolean; }; }; externals: string[]; }) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
-    config.externals.push('pino-pretty', 'encoding');
+    config.externals.push('pino-pretty', 'encoding', 'lokijs', '@types/lokijs');
     return config;
   },
   env: {
     NEXT_PUBLIC_CLIENT_ID: process.env.NEXT_PUBLIC_CLIENT_ID,
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**', // Allow any path on Unsplash
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
